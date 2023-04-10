cf5ae19ec0ea8489d8c84bf741514754f19a7a67
Hadi Kaddoura <hakaddou@lab3r5s7.42abudhabi.ae>
2023-04-10 19:46:39 +0400

    done

--- includes/constants.h
+++ includes/constants.h
@@ -6,7 +6,7 @@
 /*   By: hakaddou <hakaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:15:27 by hakaddou          #+#    #+#             */
-/*   Updated: 2023/04/10 19:44:39 by hakaddou         ###   ########.fr       */
+/*   Updated: 2023/04/10 19:46:26 by hakaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@
 # define PI 3.14159265
 # define TWO_PI 6.283185
 
-# define FOV_ANGLE 120 * (PI / 180)
+# define FOV_ANGLE 60 * (PI / 180)
 
 #define WALL_STRIP_WIDTH 1
 
