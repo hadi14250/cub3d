280d618e2a26465d90cb67f9b93a263cc5887bf3
bsaeed <bsaeed@student.42.fr>
2023-04-12 20:37:24 +0400

    Checking player on border need to fix invalid read

--- parsing/parse_mapfile.c
+++ parsing/parse_mapfile.c
@@ -6,7 +6,7 @@
 /*   By: bsaeed <bsaeed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 23:03:02 by bsaeed            #+#    #+#             */
-/*   Updated: 2023/04/10 19:49:29 by bsaeed           ###   ########.fr       */
+/*   Updated: 2023/04/12 20:33:02 by bsaeed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,26 @@ void	parse_map(t_cub *cub)
 	check_player_format(cub);
 }
 
+void	check_player_on_border(t_cub *cub)
+{
+	int height;
+	int width;
+	int i;
+
+	i = 0;
+	width = get_longest_line(cub->map) - 1;
+	height = return_split_len(cub->map);
+	while (i < height)
+	{
+		if (cub->map[i][width] == 'N' || cub->map[i][0] == 'N'
+			|| cub->map[i][width] == 'E' || cub->map[i][0] == 'E'
+			|| cub->map[i][width] == 'S' || cub->map[i][0] == 'S'
+			|| cub->map[i][width] == 'W' || cub->map[i][0] == 'W')
+			exit_cub(cub, 1, "Error\nPlayer position on border\n");
+		i++;
+	}
+}
+
 void	validations(t_cub *cub)
 {
 	char	**temp;
@@ -56,6 +76,7 @@ void	validations(t_cub *cub)
 	temp = allocate_new_map(cub);
 	memset_map(cub, temp);
 	hadis_rectangle_map(cub, temp);
+	check_player_on_border(cub);
 	print_map_two(cub->map);
 }
 
