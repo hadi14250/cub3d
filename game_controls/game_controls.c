336282336a861c684f40925e6b613042cc1ceb75
hadi14250 <hadihk2k@gmail.com>
2024-09-24 10:30:42 +0400

    now the box exactly prints in the middle of the screen, task for tmrw is make the string print exactly in the middle of the box and make the string disapper when viewing minimap in full

--- game_controls/game_controls.c
+++ game_controls/game_controls.c
@@ -6,7 +6,7 @@
 /*   By: hakaddou <hakaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 09:50:53 by hakaddou          #+#    #+#             */
-/*   Updated: 2024/09/24 10:13:43 by hakaddou         ###   ########.fr       */
+/*   Updated: 2024/09/24 10:25:33 by hakaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,15 @@
 
 void    print_control_box(t_cub *cub)
 {
+    int     width = WINDOW_WIDTH / 2;
+    int     height = WINDOW_HEIGHT / 2;
+    int x = WINDOW_WIDTH /  4;
+    int y = WINDOW_HEIGHT / 4;
     t_rect controls_rect;
-    controls_rect = init_rect(500, 500, 500, 500);
+
+    controls_rect = init_rect(x, y, width, height);
     draw_rectangle(&cub->img, controls_rect, BLACK_COLOR);
     render(cub);
-    // mlx_string_put(cub->mlx, cub->win, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, BLACK_COLOR, "Hello Pretty World");
+    // maybe use strlen to make the string actually print in the middle
+    mlx_string_put(cub->mlx, cub->win, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WHITE_COLOR, "Hello Pretty World");
 }
