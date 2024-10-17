91023959872ab30e0407243797b2ca3454da96f1
hadi14250 <hadihk2k@gmail.com>
2024-10-17 08:57:02 +0400

    added 'Change Sky Color (random)' button functionality

--- game_controls/activate_button.c
+++ game_controls/activate_button.c
@@ -6,7 +6,7 @@
 /*   By: hakaddou <hakaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 21:18:21 by hakaddou          #+#    #+#             */
-/*   Updated: 2024/10/17 08:51:28 by hakaddou         ###   ########.fr       */
+/*   Updated: 2024/10/17 08:54:42 by hakaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,17 @@ void    change_ground_color_random(t_cub *cub)
     cub->floor = color;
 }
 
+void    change_sky_color_random(t_cub *cub)
+{
+    unsigned int red = rand() % 256;
+    unsigned int green = rand() % 256;
+    unsigned int blue = rand() % 256;
+
+    unsigned int color = (red << 16) | (green << 8) | blue;
+    cub->ceiling = color;
+}
+
+
 void    parse_button_functionality(t_button *hovered, t_cub *cub)
 {
     if(!ft_strncmp(hovered->str, "Exit Game", ft_strlen(hovered->str)))
@@ -47,6 +58,8 @@ void    parse_button_functionality(t_button *hovered, t_cub *cub)
     }
     if(!ft_strncmp(hovered->str, "Change Ground Color (random)", ft_strlen(hovered->str)))
         change_ground_color_random(cub);
+    if(!ft_strncmp(hovered->str, "Change Sky Color (random)", ft_strlen(hovered->str)))
+        change_sky_color_random(cub);
 }
 
 void    activate_button(t_cub *cub)
