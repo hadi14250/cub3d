3031a854d54453408c2fd59850577e56fb9ea016
hadi14250 <hadihk2k@gmail.com>
2024-10-26 09:48:08 +0400

    removed change gun button

--- includes/constants.h
+++ includes/constants.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   constants.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hadikaddoura <hadikaddoura@student.42.f    +#+  +:+       +#+        */
+/*   By: hakaddou <hakaddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 20:15:27 by hakaddou          #+#    #+#             */
-/*   Updated: 2024/10/22 20:19:19 by hadikaddour      ###   ########.fr       */
+/*   Updated: 2024/10/26 09:46:48 by hakaddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@
 
 # define MINIMAP_SCALE_FACTOR 0.3
 
-# define BUTTON_NUM 10
+# define BUTTON_NUM 9
 
 # define X_MID WINDOW_WIDTH / 2
 
